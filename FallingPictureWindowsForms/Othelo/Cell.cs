78287100c5a319f4a146f3e57d80030d0af887b9
itay601 <itay601@LAPTOP-UI69H6FF>
2024-10-10 12:46:54 +0300

    Added 2 forms for othelo game one configuration for the game and the game im-self

---
+++ FallingPictureWindowsForms/Othelo/Cell.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FallingPictureWindowsForms
+{
+    internal class Cell
+    {
+        private string m_occupiedBy; // (Black, White, or None);
+        private char m_inputCell; // ' ' , O , X
+        public char m_InputCell
+        {
+            get { return this.m_inputCell; }
+            set { this.m_inputCell = value; }
+        }
+        public string m_OccupiedBy
+        {
+            get { return this.m_occupiedBy; }
+            set { this.m_occupiedBy = value; }
+        }
+        public Cell(string occupiedBy = null, char inputCell = ' ')
+        {
+            this.m_occupiedBy = occupiedBy;
+            this.m_inputCell = inputCell;
+        }
+        public void UpdateCellStatus(string i_occupiedBy, char inputCell)
+        {
+            this.m_occupiedBy = i_occupiedBy;
+            this.m_inputCell = inputCell;
+        }
+    }
+}
---
+++ FallingPictureWindowsForms/OtheloUI.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FallingPictureWindowsForms
+{
+    public class OthelloGameUI : Form
+    {
+        private Button[,] buttonGrid;
+        private Label statusLabel;
+        private int boardSize;
+
+        public OthelloGameUI(int boardSize)
+        {
+            this.boardSize = boardSize;
+            InitializeGameForm();
+        }
+        private void InitializeGameForm()
+        {
+            this.Text = "Othello Game";
+            this.Size = new Size(400, 450);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            buttonGrid = new Button[boardSize, boardSize];
+            int buttonSize = 40;
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    Button btn = new Button
+                    {
+                        Size = new Size(buttonSize, buttonSize),
+                        Location = new Point(j * buttonSize, i * buttonSize),
+                        Tag = new Point(i, j)
+                    };
+                    buttonGrid[i, j] = btn;
+                    this.Controls.Add(btn);
+                }
+            }
+
+            statusLabel = new Label
+            {
+                Location = new Point(10, boardSize * buttonSize + 10),
+                Size = new Size(this.Width - 20, 30),
+                Text = "Game Status"
+            };
+            this.Controls.Add(statusLabel);
+        }
+        
+        public void SetCellClickHandler(EventHandler handler)
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    buttonGrid[i, j].Click += handler;
+                }
+            }
+        }
+
+        public void UpdateBoardDisplay(char[,] board, bool[,] validMoves)
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    buttonGrid[i, j].Text = board[i, j].ToString();
+                    buttonGrid[i, j].Enabled = validMoves[i, j];
+                }
+            }
+        }
+
+        public void UpdateStatusLabel(string status)
+        {
+            statusLabel.Text = status;
+        }
+
+        public void ShowGameResult(string message)
+        {
+            MessageBox.Show(message, "Game Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+
+        public void Showdialog()
+        {
+            this.ShowDialog();
+        }
+    }
+}
